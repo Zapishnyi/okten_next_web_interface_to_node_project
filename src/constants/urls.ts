aa3d0664e64e66730ebad462198ff71affe92a96
Zapishnyi <zapishny@ukr.net>
2024-08-05 23:41:10 +0300

    login singup added

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,6 +1,6 @@
-export const base = new URL("https://node-test-server-nhs2.onrender.com")
+export const base = new URL("https://node-test-server-nhs2.onrender.com/")
 
 export const urls = {
-    login: "/auth/login",
-    singUp: "/auth/sing-up",
+    login: "auth/login",
+    singUp: "auth/sing-up",
 }
