9ca12ba4d20d3f302a7bef20caecd30e4d4026e3
Zapishnyi <zapishny@ukr.net>
2024-08-05 23:26:23 +0300

    login singup added

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,6 +1,6 @@
 import * as url from "node:url";
 
-export const base = new URL("https://node-test-server-nhs2.onrender.com/cars")
+export const base = new URL("https://node-test-server-nhs2.onrender.com")
 
 export const urls = {
     login: "/auth/login",
