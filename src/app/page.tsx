bb619f5d3f63e22748d25248bc8626397d537afb
Zapishnyi <zapishny@ukr.net>
2024-08-05 16:46:39 +0300

    pages update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import {ICarUpdated} from "@/interfaces/ICar";
 import CarCard from "@/components/CarCard/CarCard";
+import styles from "./page.module.css"
 
 export default async function Home() {
     const cars: ICarUpdated[] = await fetch("https://node-test-server-nhs2.onrender.com/cars").then(data => data.json())
     console.log(cars)
-    return <div>
+    return <div className={styles.pageWrapper}>
         {cars.map((e, index) => <CarCard key={index} car={e}/>)}
     </div>
 }
