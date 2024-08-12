84b92ecbd9732bab9331a7682d899d4ce33f6873
Zapishnyi <zapishny@ukr.net>
2024-08-12 19:02:49 +0300

    no cash add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default async function Home() {
     // const cars: ICarUpdated[] = await fetch("http://localhost:3001/cars").then(data => data.json())
 
     const cars = (await fetch("https://node-test-server-nhs2.onrender.com/cars").then(data => data.json())) as IPaginationResult<ICarUpdated, CarOrderByEnum>
+    console.log("Cars", cars)
     return <div className={styles.pageWrapper}>
         {cars.data && cars.data.map((e, index) => <CarCard key={index} car={e}/>)}
     </div>
