69d97304bc66dc5d4b536e5fc98b44a8423efe28
Zapishnyi <zapishny@ukr.net>
2024-08-12 19:07:02 +0300

    no cash add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import {CarOrderByEnum} from "@/enums/carOrderBy.enum";
 export default async function Home() {
     // const cars: ICarUpdated[] = await fetch("http://localhost:3001/cars").then(data => data.json())
 
-    const cars = (await fetch("https://node-test-server-nhs2.onrender.com/cars").then(data => data.json())) as IPaginationResult<ICarUpdated, CarOrderByEnum>
+    const cars = (await fetch("https://node-test-server-nhs2.onrender.com/cars", {cache: "no-store"}).then(data => data.json())) as IPaginationResult<ICarUpdated, CarOrderByEnum>
     console.log("Cars", cars)
     return <div className={styles.pageWrapper}>
         {cars.data && cars.data.map((e, index) => <CarCard key={index} car={e}/>)}
