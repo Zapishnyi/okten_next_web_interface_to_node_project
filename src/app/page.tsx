10ef2b6d33adac7dcf43e7ffac8620e64a965755
Zapishnyi <zapishny@ukr.net>
2024-08-12 19:29:57 +0300

    no cash add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
-"use client"
-import {ICarUpdated} from "@/interfaces/ICar";
 import CarCard from "@/components/CarCard/CarCard";
 import styles from "./page.module.css"
 import {IPaginationResult} from "@/interfaces/IPaginated";
+import {ICarUpdated} from "@/interfaces/ICar";
 import {CarOrderByEnum} from "@/enums/carOrderBy.enum";
-import {useEffect, useState} from "react";
 
-export default function Home() {
+export default async function Home() {
     // const cars: ICarUpdated[] = await fetch("http://localhost:3001/cars").then(data => data.json())
-    const [cars, setCars] = useState<IPaginationResult<ICarUpdated, CarOrderByEnum> | null>(null)
-    useEffect(() => {
-        fetch("https://node-test-server-nhs2.onrender.com/cars", {cache: "no-store"}).then(async data => setCars(await data.json()))
-        console.log("Cars", cars)
-    }, []);
+
+    const cars = (await fetch("https://node-test-server-nhs2.onrender.com/cars", {cache: "no-store"}).then(data => data.json())) as IPaginationResult<ICarUpdated, CarOrderByEnum>
+    console.log("Cars", cars)
+
 
     return <div className={styles.pageWrapper}>
-        {cars?.data && cars.data.map((e, index) => <CarCard key={index} car={e}/>)}
+        {cars.data && cars.data.map((e, index) => <CarCard key={index} car={e}/>)}
     </div>
 
 
