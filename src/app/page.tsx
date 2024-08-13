b233745adca9a4961b3fa1e11fd070f8ef9879bb
Zapishnyi <zapishny@ukr.net>
2024-08-13 09:17:51 +0300

    cache default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import {CarOrderByEnum} from "@/enums/carOrderBy.enum";
 export default async function Home() {
     // const cars: ICarUpdated[] = await fetch("http://localhost:3001/cars").then(data => data.json())
 
-    const cars = (await fetch("https://node-test-server-nhs2.onrender.com/cars", {cache: "default"}).then(data => data.json())) as IPaginationResult<ICarUpdated, CarOrderByEnum>
+    const cars = (await fetch("https://node-test-server-nhs2.onrender.com/cars", {cache: "no-cache"}).then(data => data.json())) as IPaginationResult<ICarUpdated, CarOrderByEnum>
     console.log("Cars", cars)
 
 
